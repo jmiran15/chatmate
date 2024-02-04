cb05c4c7e5c54ebf45275411444ab1907d319cc2
Jonathan Miranda <jmiran15@jhu.edu>
2024-02-03 22:55:42 -0500

    shadcn

--- app/routes/chatbots._index.tsx
+++ app/routes/chatbots._index.tsx
@@ -2,6 +2,8 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import ChatbotCard from "~/components/ChatbotCard";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
 
 import { getChatbotsByUserId } from "~/models/chatbot.server";
 import { requireUserId } from "~/session.server";
@@ -16,12 +18,12 @@ export default function MyChatbots() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex flex-col gap-2 w-full px-24 py-12">
-      <h1 className="text-2xl font-bold">My Chatbots</h1>
-      <Link
-        to="new"
-        className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600 self-end"
-      >
+    <div className="flex flex-col gap-2 w-full py-12">
+      <h1 className="text-3xl font-bold leading-tight tracking-tighter">
+        My Chatbots
+      </h1>
+
+      <Link to="new" className={cn(buttonVariants(), "self-end")}>
         + New Chatbot
       </Link>
 
