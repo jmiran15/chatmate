6f7c8e3cad33374ce6603dd5f78b704034f0ee56
Jonathan Miranda <jmiran15@jhu.edu>
2024-02-03 22:36:49 -0500

    some shadcn

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 // should we have the header here? since this is the root layout??? we can get the user in the loader and change header accordingly
 
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
@@ -16,10 +15,9 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 
-import "@mantine/core/styles.css";
-
 import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
+import { SiteHeader } from "./components/site-header";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -40,53 +38,51 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
-        <ColorSchemeScript />
       </head>
       <body className="min-h-screen h-full flex flex-col">
-        <MantineProvider>
-          <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-            <h1 className="text-3xl font-bold">
-              <Link to=".">Home</Link>
-            </h1>
-            {data.user ? (
-              <div className="flex items-center justify-center gap-x-4">
-                <Link
-                  to="/chatbots"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+        <SiteHeader />
+        {/* <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+          <h1 className="text-3xl font-bold">
+            <Link to=".">Home</Link>
+          </h1>
+          {data.user ? (
+            <div className="flex items-center justify-center gap-x-4">
+              <Link
+                to="/chatbots"
+                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+              >
+                View Chatbots for {data.user.email}
+              </Link>
+              <Form action="/logout" method="post">
+                <button
+                  type="submit"
+                  className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
                 >
-                  View Chatbots for {data.user.email}
-                </Link>
-                <Form action="/logout" method="post">
-                  <button
-                    type="submit"
-                    className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-                  >
-                    Logout
-                  </button>
-                </Form>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center gap-x-4">
-                <Link
-                  to="/join"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                >
-                  Sign up
-                </Link>
-                <Link
-                  to="/login"
-                  className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
-                >
-                  Log In
-                </Link>
-              </div>
-            )}
-          </header>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </MantineProvider>
+                  Logout
+                </button>
+              </Form>
+            </div>
+          ) : (
+            <div className="flex items-center justify-center gap-x-4">
+              <Link
+                to="/join"
+                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+              >
+                Sign up
+              </Link>
+              <Link
+                to="/login"
+                className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
+              >
+                Log In
+              </Link>
+            </div>
+          )}
+        </header> */}
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,32 +1,34 @@
 // this is the landing page
-import { Stack, Text, Center, Title } from "@mantine/core";
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
-
-import { useOptionalUser } from "~/utils";
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "~/components/page-header";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
-  const user = useOptionalUser();
   return (
-    <Center px={300} py={100}>
-      <Stack align="flex-start">
-        <Title order={1}>Create and discover new chatbots</Title>
-        <Text size="lg" color="dimmed">
-          With chatmate.dev you can combine multiple gpt4 chatbots with vector
-          document retrievals to create complex multi-chain chatbots. You can
-          share your chatbots and discover new ones from the community.
-        </Text>
-        {/* {user && (
-          <Link
-            to="/chatbots"
-            className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-          >
-            View Chatbots for {user.email}
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading>
+          10x your landing page conversion with a custom chatbot widget
+        </PageHeaderHeading>
+        <PageHeaderDescription>
+          Join thousands of startups and enterprises who use Chatmate’s platform
+          to increase their conversion rates
+        </PageHeaderDescription>
+        <PageActions>
+          <Link to="/join" className={cn(buttonVariants())}>
+            Get Started
           </Link>
-        )} */}
-      </Stack>
-    </Center>
+        </PageActions>
+      </PageHeader>
+    </div>
   );
 }
