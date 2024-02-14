8a0463f90bc042fee237707f2b04fb3bdf132860
Jonathan Miranda <jmiran15@jhu.edu>
2024-02-14 04:17:03 -0500

    change title and favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ import { FAQ } from "~/components/landing-page/faq";
 import { Cta } from "~/components/landing-page/cta";
 import { Navbar } from "~/components/navbar";
 
-export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
+export const meta: MetaFunction = () => [{ title: "Chatmate" }];
 
 export default function Index() {
   return (
