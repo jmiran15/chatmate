ad25bad8b22ac5600ce351d2087c775ad2a9a413
Jonathan Miranda <jmiran15@jhu.edu>
2024-02-17 01:13:54 -0500

    testing cross site cookies

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -2,4 +2,6 @@ import { createCookie } from "@remix-run/node"; // or cloudflare/deno
 
 export const widgetChat = createCookie("widgetChat", {
   maxAge: 604_800, // one week
+  secure: true,
+  sameSite: "none",
 });
