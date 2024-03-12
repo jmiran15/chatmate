ea7402db06a5279fbfb69349407fdccf97b0b443
Jonathan Miranda <jmiran15@jhu.edu>
2024-03-11 22:27:39 -0400

    added css to public

---
+++ app/routes/buttontest.tsx
@@ -0,0 +1,11 @@
+import { Button } from "~/components/ui/button";
+
+export default function TEST() {
+  return (
+    <div className="m-auto h-screen w-screen">
+      <Button type="submit" variant={"destructive"}>
+        Click me
+      </Button>
+    </div>
+  );
+}
