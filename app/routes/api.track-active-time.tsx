37718d79f48cdf323c354918323ab89cc99fcd04
Jonathan Miranda <jmiran15@jhu.edu>
2024-07-26 18:53:24 -0400

    api route to update elapsed time

---
+++ app/routes/api.track-active-time.tsx
@@ -0,0 +1,58 @@
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { prisma } from "~/db.server";
+
+export async function loader() {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  return json(
+    { success: true },
+    {
+      headers,
+    },
+  );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const { sessionId, activeTime } = await request.json();
+
+  const chat = await prisma.chat.findFirst({
+    where: {
+      sessionId: sessionId,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  });
+
+  if (!chat) {
+    throw new Error("Chat not found");
+  }
+
+  const updatedChat = await prisma.chat.update({
+    where: {
+      id: chat.id,
+    },
+    data: {
+      elapsedMs: String(activeTime),
+    },
+  });
+
+  console.log("api.track-active-time - updatedChat: ", updatedChat);
+
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  return json(
+    { updatedChat, success: true },
+    {
+      headers,
+    },
+  );
+}
