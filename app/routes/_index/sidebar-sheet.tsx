fc86cc1e8ea377c6ee2096cf86f16b0a6a69d0de
Jonathan Miranda <jmiran15@jhu.edu>
2024-08-01 00:34:51 -0400

    fix a bug in prod from vite-env-only

--- app/routes/_index/sidebar-sheet.tsx
+++ app/routes/_index/sidebar-sheet.tsx
@@ -1,30 +1,16 @@
 import { Sheet, SheetContent } from "../../components/ui/sheet";
 import { Button } from "../../components/ui/button";
-import { Link, useLocation, useMatches, useParams } from "@remix-run/react";
-import { cn, isActive } from "~/lib/utils";
-import { LucideIcon, Menu } from "lucide-react";
+import { Link, useLocation } from "@remix-run/react";
+import { Menu } from "lucide-react";
 import { Icons } from "../../components/icons";
 import { useEffect, useState } from "react";
-import { Badge } from "../../components/ui/badge";
+import { RouteLink, useLinks } from "./use-links";
+import SidebarLink from "./sidebar-link";
 
-interface RouteProps {
-  path: string;
-  title: string;
-  icon?: LucideIcon;
-  navigate?: boolean;
-  badge?: number;
-}
-
-export default function SidebarSheet({
-  routes,
-  matches,
-}: {
-  routes: RouteProps[];
-  matches: ReturnType<typeof useMatches>;
-}) {
+export default function SidebarSheet() {
   const [open, setOpen] = useState(false);
-  const { chatbotId } = useParams();
   const location = useLocation();
+  const { routes } = useLinks();
 
   useEffect(() => {
     setOpen(false);
@@ -55,51 +41,9 @@ export default function SidebarSheet({
             <span className="sr-only">Chatmate</span>
           </Link>
 
-          {routes.map((link, index) =>
-            link.navigate ? (
-              <Link
-                id={link.path}
-                key={`${link.path}-${index}`}
-                to={link.path}
-                preventScrollReset={!link.navigate}
-                className={cn(
-                  isActive({
-                    matches,
-                    path: link.path,
-                    chatbotId,
-                  })
-                    ? "mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                    : "mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground",
-                )}
-              >
-                {link.icon ? <link.icon className="h-5 w-5" /> : null}
-                {link.title}
-                {link.badge ? (
-                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    {link.badge}
-                  </Badge>
-                ) : null}
-              </Link>
-            ) : (
-              <div
-                id={link.path}
-                key={`${link.path}-${index}`}
-                onClick={() => (window.location.href = link.path)}
-                className={cn(
-                  isActive({
-                    matches,
-                    path: link.path,
-                    chatbotId,
-                  })
-                    ? "mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                    : "mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground",
-                )}
-              >
-                {link.icon ? <link.icon className="h-5 w-5" /> : null}
-                {link.title}
-              </div>
-            ),
-          )}
+          {routes.map((link, index) => (
+            <SidebarLink key={`${link.path}-${index}`} link={link} isMobile />
+          ))}
         </nav>
       </SheetContent>
     </Sheet>
