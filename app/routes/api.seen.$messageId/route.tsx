e36bd295824f8c2ad9ca749d92af209575f114f9
Jonathan Miranda <jmiran15@jhu.edu>
2024-09-05 13:59:39 -0400

    cors loader to /api/seen

--- app/routes/api.seen.$messageId/route.tsx
+++ app/routes/api.seen.$messageId/route.tsx
@@ -2,6 +2,21 @@
 import { ActionFunctionArgs, json } from "@remix-run/node";
 import { prisma } from "~/db.server";
 
+export async function loader() {
+  const corsHeader =
+    process.env.NODE_ENV === "production"
+      ? {
+          "Access-Control-Allow-Origin": "*",
+        }
+      : {};
+  const headers = {
+    ...corsHeader,
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  } as HeadersInit;
+  return json({ headers });
+}
+
 export const action = async ({ params, request }: ActionFunctionArgs) => {
   const { messageId } = params;
 
